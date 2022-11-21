9d9ffdfe3279e5c6688cbdc8f3d82d063da65cb4
Paulo Salvatore <salvatoregames@gmail.com>
2022-11-21 18:24:04 -0300

    chore(frontend): create project using vite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,34 @@
-import Game from "./components/Game/Game";
-import { useState } from "react";
-import HighScore from "./components/HighScore/HighScore";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
-export default function App() {
-  const [isGameOver, setIsGameOver] = useState(false);
-
-  function onDie(score) {
-    setIsGameOver(true);
-  }
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Game onDie={onDie} />
-      {isGameOver && <HighScore />}
-    </>
-  );
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
 }
+
+export default App
